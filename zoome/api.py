8e9b35be6e4e59b263a83088f802bf384110deb8
Vladislav E. Gusarov <programm00@ya.ru>
2021-01-12 22:13:50 +0300

    add: jwt token generate function

--- zoome/api.py
+++ zoome/api.py
@@ -1,8 +1,22 @@
+import jwt
+
+
 class ZoomClient:
     def __init__(self, api_key: str, secret_api_key: str):
         self.api_key = api_key
         self.secret_api_key = secret_api_key
+        self.jwt_token = self.generate_jwt_token()
+
+    def generate_jwt_token(self):
+        header = {"alg": "HS256", "typ": "JWT"}
+        payload = {"iss": self.api_key, "exp": 1496091964000}
+        signature = jwt.encode(payload, self.secret_api_key, algorithm="HS256", headers=header)
+        return signature
+
+    def get_conferences_list(self):
+        pass
 
 
 if __name__ == '__main__':
     print('use this code like import module')
+
