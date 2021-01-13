6f0ddb0d588299033bd6b39918fef32ff55e24e7
Vladislav E. Gusarov <programm00@ya.ru>
2021-01-13 07:22:04 +0300

    add: parameter expired_time to generate_jwt_token function

--- zoome/api.py
+++ zoome/api.py
@@ -2,14 +2,15 @@ import jwt
 
 
 class ZoomClient:
-    def __init__(self, api_key: str, secret_api_key: str):
+    def __init__(self, api_key: str, secret_api_key: str, expired_time: int = 5400):
         self.api_key = api_key
         self.secret_api_key = secret_api_key
-        self.jwt_token = self.generate_jwt_token()
+        self.expired_time = expired_time
+        self.jwt = self.generate_jwt_token(self.expired_time)
 
-    def generate_jwt_token(self):
+    def generate_jwt_token(self, exp: int = 1496091964000):
         header = {"alg": "HS256", "typ": "JWT"}
-        payload = {"iss": self.api_key, "exp": 1496091964000}
+        payload = {"iss": self.api_key, "exp": exp}
         signature = jwt.encode(payload, self.secret_api_key, algorithm="HS256", headers=header)
         return signature
 
@@ -19,4 +20,3 @@ class ZoomClient:
 
 if __name__ == '__main__':
     print('use this code like import module')
-
