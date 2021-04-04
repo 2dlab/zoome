82a781357592ce02628940e0f2d0298243a81cfe
Zimovik007 <programm00@ya.ru>
2021-04-04 22:41:03 +0300

    collect more meta from zoom meetings

--- zoome/utils.py
+++ zoome/utils.py
@@ -7,7 +7,9 @@ def get_download_urls_from_meeting(meeting):
         return {
             "download_url": record["download_url"],
             "recording_type": record["recording_type"],
-            "file_type": record["file_type"]
+            "file_type": record["file_type"],
+            "recording_start": record["recording_start"],
+            "recording_end": record["recording_end"]
         }
 
     return [get_url_obj(record) for record in recording_files]
